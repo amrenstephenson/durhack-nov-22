e321d9dbec4835d05607ec09e5180161a6614918
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2022-11-19 12:40:42 +0000

    Create basic server

---
+++ server/server.py
@@ -0,0 +1,32 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+HOST_NAME = "localhost"
+SERVER_PORT = 8080
+
+
+class DurhackServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/api" or self.path.startswith("/api/"):
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(f"It's the API!", "utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(f"<h1>ERROR 404</h1>{self.path} not found.", "utf-8"))
+
+
+if __name__ == "__main__":
+    durhack_server = HTTPServer((HOST_NAME, SERVER_PORT), DurhackServer)
+    print(f"Server started at http://{HOST_NAME}:{SERVER_PORT}")
+
+    try:
+        durhack_server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    durhack_server.server_close()
+    print("Server stopped.")
