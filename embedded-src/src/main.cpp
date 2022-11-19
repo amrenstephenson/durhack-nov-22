2fcc9d84f617f6b11f88ae409ae182ac09a7a3f5
Tommy Muir <tcmuir03@gmail.com>
2022-11-19 13:07:42 +0000

    Initial embedded application

---
+++ embedded-src/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  Serial.printf("Hello world\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
