d02c228bdeb021808cbcba8f6122b57495bac97e
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2022-11-19 16:40:46 +0000

    Add basic api endpoints

--- server/server.py
+++ server/server.py
@@ -1,40 +1,57 @@
+import importlib
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
+import sys
+import json
+
+
+SCRIPT_DIR = os.path.dirname(__file__)
+visualisation_path = os.path.join(SCRIPT_DIR, "..", "visualisation", "visualisation.py")
+
+loader = importlib.machinery.SourceFileLoader("visualisation", visualisation_path)
+spec = importlib.util.spec_from_loader("visualisation", loader)
+visualisation = importlib.util.module_from_spec(spec)
+loader.exec_module(visualisation)
 
 
 HOST_NAME = "0.0.0.0"
 SERVER_PORT = 8080
 
-ALLOWED_PATHS = ["dashboard.html"]
+ALLOWED_PATHS = ["dashboard.html", "api.html"]
+
+vis = visualisation.Visualisation()
 
 
 class DurhackServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/api" or self.path.startswith("/api/"):
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes(f"It's the API!", "utf-8"))
-        elif self.path == "/":
+        path = self.path
+        path = path[1:]  # Remove leading /
+        path = path[:-1] if path.endswith("/") else path  # Remove trailing / (if there is one)
+        if path == "api/currencies/num":
+            self.good_response(str(len(vis.currency)))
+        elif path == "api/currencies/raw":
+            self.good_response(json.dumps(vis.currency))
+        elif path == "":
             self.send_response(301)
-            self.send_header('Location', '/dashboard')
+            self.send_header("Location", "/dashboard")
             self.end_headers()
         else:
-            self.path = self.path[1:]  # Remove leading /
-            self.path = self.path[:-1] if self.path.endswith("/") else self.path  # Remove trailing / (if there is one)
-            self.path = self.path + ".html" if "." not in self.path else self.path  # Default to .html if no file type is given.
+            path = path + ".html" if "." not in path else path  # Default to .html if no file type is given.
 
-            if self.path in ALLOWED_PATHS:
-                with open(os.path.join(os.path.dirname(__file__), "public", self.path), "r") as f:
-                    self.send_response(200)
-                    self.send_header("Content-type", "text/html")
-                    self.end_headers()
-                    self.wfile.write(bytes(f.read(), "utf-8"))
+            if path in ALLOWED_PATHS:
+                with open(os.path.join(SCRIPT_DIR, "public", path), "r") as f:
+                    self.good_response(f.read())
             else:
                 self.send_response(404)
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
-                self.wfile.write(bytes(f"<h1>ERROR 404</h1>{self.path} not found.", "utf-8"))
+                self.wfile.write(bytes(f"<h1>ERROR 404</h1>{path} not found.", "utf-8"))
+
+    def good_response(self, text):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(str(text), "utf-8"))
 
 
 if __name__ == "__main__":
--- visualisation/visualisation.py
+++ visualisation/visualisation.py
@@ -5,44 +5,39 @@ import json
 import random
 import threading
 
-STREAM_SYBMOL = "btcusdt"
-STREAM_TYPE = "aggTrade"
-#STREAM_NAME = STREAM_SYBMOL + "@" + STREAM_TYPE
-STREAM_NAME = "!ticker_1d@arr"
 
-ENDPOINT = f"wss://stream.binance.com:9443/ws/{STREAM_NAME}"
+class Visualisation():
+    # STREAM_SYBMOL = "btcusdt"
+    # STREAM_TYPE = "aggTrade"
+    # STREAM_NAME = STREAM_SYBMOL + "@" + STREAM_TYPE
+    STREAM_NAME = "!ticker_1d@arr"
 
-running = True
+    ENDPOINT = f"wss://stream.binance.com:9443/ws/{STREAM_NAME}"
 
-currency = {}
+    def __init__(self, debug=False):
+        self.debug = debug
+        self.currency = {}
 
-def main():
-    threading.Thread(target=wait_for_input_then_stop).start()
-    asyncio.new_event_loop().run_until_complete(stream())
+        threading.Thread(target=self.start_event_loop).start()
 
+    def start_event_loop(self):
+        asyncio.new_event_loop().run_until_complete(self.stream())
 
-def wait_for_input_then_stop():
-    global running
-    input()
-    running = False
-    print("Stopping!")
-
-
-async def stream():
-    global running
-    async with connect(ENDPOINT) as websocket:
-        while running:
-            result = await websocket.recv()
-            if running:
+    async def stream(self):
+        global running
+        async with connect(self.ENDPOINT) as websocket:
+            while True:
+                result = await websocket.recv()
                 data = json.loads(result)
                 for coin in data:
-                  if coin['s'] not in currency:
-                    print(coin['s'], coin['h'])
-                  
-                  currency[coin['s']] = coin['h']
+                    if self.debug and coin['s'] not in self.currency:
+                        print(coin['s'], coin['h'])
 
-                print(len(currency))
+                    self.currency[coin['s']] = coin['h']
+
+                if self.debug:
+                    print(len(self.currency))
 
 
 if __name__ == "__main__":
-    main()
+    Visualisation(debug=True)
