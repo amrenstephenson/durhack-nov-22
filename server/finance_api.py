4d478f16d9f164b657cb5334014b73ec0589ee90
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2022-11-20 09:53:24 +0000

    Add extra error handling

--- server/finance_api.py
+++ server/finance_api.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 import requests
-from typing import Tuple
+from typing import Tuple, Optional
+
+
+class SymbolSplittingError(Exception):
+    pass
 
 
 class FinanceAPI(ABC):
@@ -8,19 +12,25 @@ class FinanceAPI(ABC):
         super().__init__()
 
     @abstractmethod
-    def split_symbol_pair(self, symsymbol_pairbol: str) -> Tuple[str, str]:
+    def split_symbol_pair(self, symsymbol_pairbol: str) -> Optional[Tuple[str, str]]:
         pass
 
     @abstractmethod
-    def collect_candlestick_data(self, symbol_pair: str):
+    def collect_candlestick_data(self, symbol_pair: str) -> Optional[str]:
         pass
 
 
 class FinanceAPIBinance(FinanceAPI):
-    def split_symbol_pair(self, symbol_pair: str):
-        resp = requests.get("https://api.binance.com/api/v3/exchangeInfo", params={"symbol": symbol_pair})
+    def split_symbol_pair(self, symbol_pair: str) -> Optional[Tuple[str, str]]:
+        try:
+            resp = requests.get("https://api.binance.com/api/v3/exchangeInfo", params={"symbol": symbol_pair})
+        except requests.exceptions.ConnectionError:
+            raise SymbolSplittingError
         symbols = resp.json()["symbols"][0]
         return (symbols["baseAsset"], symbols["quoteAsset"])
 
-    def collect_candlestick_data(self, symbol_pair: str):
-        return requests.get(f"https://www.binance.com/api/v3/klines?symbol={symbol_pair}&interval=1h&limit=24").json()
+    def collect_candlestick_data(self, symbol_pair: str) -> Optional[str]:
+        try:
+            return requests.get(f"https://www.binance.com/api/v3/klines?symbol={symbol_pair}&interval=1h&limit=24").json()
+        except requests.exceptions.ConnectionError:
+            return None
